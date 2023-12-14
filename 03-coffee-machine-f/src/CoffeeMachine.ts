6e2be9f127605c155d74088fa4c46de484ffb288
trikitrok <trikitrok@gmail.com>
2023-12-14 09:59:51 +0100

    making class private

--- 03-coffee-machine-f/src/CoffeeMachine.ts
+++ 03-coffee-machine-f/src/CoffeeMachine.ts
@@ -1,8 +1,8 @@
 import {DrinkMaker800} from "./DrinkMaker800";
-import {OrderProcessing} from "./OrderProcessing";
 import {Model800DrinkMakerDriver} from "./Model800DrinkMakerDriver";
 import {Drink} from "./Drink";
 import { DrinkMakerDriver } from "./DrinkMakerDriver";
+import { Order } from "./Order";
 
 export class CoffeeMachine {
     private orderProcessing: OrderProcessing;
@@ -67,5 +67,63 @@ export class CoffeeMachine {
 
     private resetOrderProcessing(): void {
         this.orderProcessing = new OrderProcessing(this.priceTable);
+    }
+}
+
+class OrderProcessing {
+    private readonly MAX_SPOONS_OF_SUGAR: number = 2;
+    private selectedDrink: Drink;
+    private extraHot: boolean;
+    private spoonsOfSugars: number;
+    private money: number;
+    private readonly priceTable: Record<Drink, number>;
+
+    constructor(priceTable: Record<Drink, number>) {
+        this.priceTable = priceTable;
+        this.money = 0;
+        this.spoonsOfSugars = 0;
+        this.extraHot = false;
+    }
+
+    isOrderReady(): boolean {
+        return this.selectedDrink !== undefined;
+    }
+
+    selectDrink(selectedDrink: Drink): void {
+        this.selectedDrink = selectedDrink;
+    }
+
+    isExtraHot(): void {
+        if (this.selectedDrink === Drink.OrangeJuice) {
+            return;
+        }
+        this.extraHot = true;
+    }
+
+    addOneSpoonOfSugar(): void {
+        this.spoonsOfSugars = Math.min(
+            this.spoonsOfSugars + 1,
+            this.MAX_SPOONS_OF_SUGAR
+        );
+    }
+
+    createOrder(): Order {
+        return new Order(this.selectedDrink, this.extraHot, this.spoonsOfSugars);
+    }
+
+    addMoney(amount: number): void {
+        this.money += amount;
+    }
+
+    computeMissingMoney(): number {
+        return this.getSelectedDrinkPrice() - this.money;
+    }
+
+    isThereEnoughMoney(): boolean {
+        return this.money >=  this.getSelectedDrinkPrice();
+    }
+
+    private getSelectedDrinkPrice(): number {
+        return this.priceTable[this.selectedDrink];
     }
 }
